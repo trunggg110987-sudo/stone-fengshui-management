f4dfbc0990804007cc2849dc5277808c6d30f0f0
Nguyenduoc04 <duocnguyenphuoc159@gmail.com>
2026-04-13 01:34:59 +0700

    update model.stone suitable for databases

--- src/main/java/com/example/stonefengshuimanagement/model/entity/Stone.java
+++ src/main/java/com/example/stonefengshuimanagement/model/entity/Stone.java
@@ -4,12 +4,40 @@ import java.math.BigDecimal;
 
 public class Stone {
     private int id;
+    private int categoryId;
     private String name;
-    private BigDecimal price;
+    private String code;
+    private double price;
     private String imageUrl;
+    private String description;
+    private int status;
 
     public Stone() {
     }
+
+    public Stone(int id, int categoryId, String name, String code, double price, String imageUrl, String description, int status) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.name = name;
+        this.code = code;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.status = status;
+    }
+
+    // 🔹 Constructor không có id (dùng khi insert)
+    public Stone(int categoryId, String name, String code,
+                 double price, String imageUrl, String description, int status) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.code = code;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.status = status;
+    }
+
 
     public int getId() {
         return id;
@@ -17,6 +45,14 @@ public class Stone {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
     }
 
     public String getName() {
@@ -27,11 +63,19 @@ public class Stone {
         this.name = name;
     }
 
-    public BigDecimal getPrice() {
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
@@ -41,5 +85,21 @@ public class Stone {
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 }
