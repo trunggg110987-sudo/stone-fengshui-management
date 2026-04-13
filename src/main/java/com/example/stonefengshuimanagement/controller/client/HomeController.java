e85fdcd84ce47fe2afc70623cef7e6b0b998d19c
CJ1024-lethingocanh <ngoc.ltnanh@gmail.com>
2026-04-14 03:56:33 +0700

    update HomeController

--- src/main/java/com/example/stonefengshuimanagement/controller/client/HomeController.java
+++ src/main/java/com/example/stonefengshuimanagement/controller/client/HomeController.java
@@ -1,4 +1,25 @@
 package com.example.stonefengshuimanagement.controller.client;
 
-public class HomeController {
+
+import com.example.stonefengshuimanagement.dao.CategoryDAO;
+import com.example.stonefengshuimanagement.model.entity.Category;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "HomeController", urlPatterns = "/home")
+public class HomeController extends HttpServlet {
+    private final CategoryDAO categoryDAO = new CategoryDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Category> categoryList = categoryDAO.findAll() ;
+        req.setAttribute("categoryList", categoryList);
+        req.getRequestDispatcher("/WEB-INF/views/client/home.jsp").forward(req, resp);
+    }
 }
