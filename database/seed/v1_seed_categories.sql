2f55358d66dce874480473f24df375b1730e91a1
WanBi <anhnbt.it@gmail.com>
2026-04-13 19:04:09 +0700

    Add data seed for categories

---
+++ database/seed/v1_seed_categories.sql
@@ -0,0 +1,6 @@
+INSERT INTO categories (name, description, status) VALUES
+                                                       ('Vòng tay', 'Vòng tay đá phong thủy giúp cân bằng năng lượng và thu hút may mắn', 1),
+                                                       ('Đá viên', 'Đá phong thủy dạng viên nhỏ, dễ mang theo bên người', 1),
+                                                       ('Mặt dây chuyền', 'Trang sức phong thủy mang ý nghĩa bảo hộ và tài lộc', 1),
+                                                       ('Quả cầu đá', 'Biểu tượng của sự viên mãn, hài hòa năng lượng', 1),
+                                                       ('Tượng đá mini', 'Vật phẩm phong thủy để bàn giúp tăng vượng khí', 1);
