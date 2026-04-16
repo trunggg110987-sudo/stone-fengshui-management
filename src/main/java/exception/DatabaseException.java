fb31016bcc78eb8183a6ea592323785a8a8f86dc
CJ1024-lethingocanh <ngoc.ltnanh@gmail.com>
2026-04-17 03:41:41 +0700

    add DatabaseException

---
+++ src/main/java/exception/DatabaseException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class DatabaseException extends Exception {
+    public DatabaseException(String message) {
+        super(message);
+    }
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
