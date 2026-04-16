eb956ae4889120ff1d73797525659c064879c1a0
CJ1024-lethingocanh <ngoc.ltnanh@gmail.com>
2026-04-17 03:45:23 +0700

    add ExceptionUtil

--- src/main/java/exception/ExceptionUtil.java
+++ src/main/java/exception/ExceptionUtil.java
@@ -1,4 +1,7 @@
 package exception;
 
 public class ExceptionUtil {
+    public static void throwDatabaseException(Exception e) throws DatabaseException {
+        throw new DatabaseException("Lỗi thao tác database", e);
+    }
 }
