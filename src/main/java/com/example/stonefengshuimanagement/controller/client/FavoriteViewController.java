55505359a7b0ce7f76f055b22bd4a78cb28612f8
Trung <trung@example.com>
2026-04-21 22:23:17 +0700

    update favorite view controller

---
+++ src/main/java/com/example/stonefengshuimanagement/controller/client/FavoriteViewController.java
@@ -0,0 +1,35 @@
+package com.example.stonefengshuimanagement.controller.client;
+
+import com.example.stonefengshuimanagement.model.entity.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "FavoriteViewController", urlPatterns = "/favorite/view")
+public class FavoriteViewController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        HttpSession session = req.getSession();
+
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            resp.sendRedirect(req.getContextPath() + "/admin/login");
+            return;
+        }
+
+        req.setAttribute("contentPage", "/views/client/favorite.jsp");
+        req.setAttribute("pageTitle", "Yêu thích");
+
+        req.getRequestDispatcher("/views/common/client-layout.jsp")
+                .forward(req, resp);
+    }
+}
