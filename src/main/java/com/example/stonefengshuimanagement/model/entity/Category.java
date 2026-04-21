e9135c7645a955d6d7d583b07b246fc7e0a2b7b5
Trung <trung@example.com>
2026-04-21 21:57:48 +0700

    create Catetory.java

--- src/main/java/com/example/stonefengshuimanagement/model/entity/Category.java
+++ src/main/java/com/example/stonefengshuimanagement/model/entity/Category.java
@@ -1,4 +1,65 @@
 package com.example.stonefengshuimanagement.model.entity;
 
 public class Category {
+    private int id;
+    private String name;
+    private String description;
+    private int status;
+
+    public Category(int id, String name, String description, int status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Category(String name, String description, int status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Category() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
