ac30a9fd54a574b9bfd52225feba0391aa0bd6db
Trung <trung@example.com>
2026-04-21 21:48:45 +0700

    update exception

--- src/main/java/com/example/stonefengshuimanagement/exception/ExceptionUtil.java
+++ src/main/java/com/example/stonefengshuimanagement/exception/ExceptionUtil.java
@@ -1,4 +1,7 @@
 package com.example.stonefengshuimanagement.exception;
 
 public class ExceptionUtil {
+    public static void throwDatabaseException(Exception e) throws DatabaseException {
+        throw new DatabaseException("loi thao tac db" , e);
+    }
 }
